57dcabd1b7ee6c2d6233071f1ddd0a099c29e840
Kevin Noutsawo <kevnoutsawo@yahoo.fr>
2023-09-04 16:48:27 +0100

    Updated google analytics id

--- gatsby-config.js
+++ gatsby-config.js
@@ -145,7 +145,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: 'UA-45666519-2',
+        trackingId: 'G-9HG5EDRYZ9',
       },
     },
     {
